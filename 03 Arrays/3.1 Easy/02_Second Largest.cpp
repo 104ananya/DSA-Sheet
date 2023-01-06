b0849a1bdc8af5abf252f4537d80351a3355d2d6
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-01-06 23:35:15 +0530

    Create 02_Second Largest.cpp

---
+++ 03 Arrays/3.1 Easy/02_Second Largest.cpp
@@ -0,0 +1,24 @@
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	    int first = -1;
+	    int second = -1;
+	    
+	    for(int i = 0; i < n; i++){
+	        
+	        if(arr[i] > first){
+	            second = first;
+	            first = arr[i];
+	        }
+	        
+	        else if(arr[i] > second && arr[i] != first){
+	            second = arr[i];
+	        }
+	    }
+	    
+	    return second;
+	}
+};
